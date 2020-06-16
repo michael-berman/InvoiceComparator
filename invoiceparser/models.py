16f285eb583dac50f50400e6639433ce7992d1a8
Michael Berman <michael.v.berman@gmail.com>
2020-06-15 21:41:04 -0400

    Finalize models

--- invoiceparser/models.py
+++ invoiceparser/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 
+class Supplier(models.Model):
+    supplier_name = models.CharField(max_length=300)
+    supplier_location = models.CharField(max_length=1000)
+
+
 class InvoiceItem(models.Model):
-    item_id = models.Index()
-    description_text = models.CharField(max_length=1000)
+    supplier_id = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)
     price = models.DecimalField()
     ship_date = models.DateTimeField()
-
-
-class Supplier(models.Model):
-    supplier_id = models.Index()
-    supplier_name = models.CharField(max_length=200)
