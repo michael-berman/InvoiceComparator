b81e7f942ce7e731813648d3ddd4f111e21bdbb0
Michael Berman <michael.v.berman@gmail.com>
2020-06-20 18:10:56 -0400

    add str method and register in admin

--- invoiceparser/admin.py
+++ invoiceparser/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Supplier, InvoiceItem
+
+admin.site.register(Supplier)
+admin.site.register(InvoiceItem)
--- invoiceparser/models.py
+++ invoiceparser/models.py
@@ -7,9 +7,15 @@ class Supplier(models.Model):
     supplier_name = models.CharField(max_length=300)
     supplier_location = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.supplier_name
+
 
 class InvoiceItem(models.Model):
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     ship_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.description
