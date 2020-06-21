06635cc2f7f59ddf80fb5822e9a57f9d48fe0b13
Michael Berman <michael.v.berman@gmail.com>
2020-06-21 14:45:40 -0400

    add index and detail views

--- invoiceparser/urls.py
+++ invoiceparser/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload, name='upload'),
-    path('supplier', views.detail, name='detail'),
-    path('<int:document_id>/', views.get_supplier, name='get_supplier')
+    path('', views.index, name='index'),
+    path('<int:supplier_id>/', views.detail, name='detail')
 ]
--- invoiceparser/views.py
+++ invoiceparser/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 from .models import Supplier
 
 # Create your views here.
 
 
-def upload(request):
-    return HttpResponse("You're looking at the upload request")
-
-
-def detail(request):
-    return HttpResponse("You're looking at the detail request")
-
-
-def get_supplier(request, document_id):
+def index(request):
     supplier_list = Supplier.objects.order_by('supplier_name')
-    output = ', '.join([s.supplier_name for s in supplier_list])
-    return HttpResponse(output)
+    context = {
+        'supplier_list': supplier_list
+    }
+    return render(request, 'invoiceparser/index.html', context)
+
+
+def detail(request, supplier_id):
+    try:
+        supplier = Supplier.objects.get(pk=supplier_id)
+    except Supplier.DoesNotExist:
+        raise Http404("Supplier does not exist")
+
+    context = {
+        'supplier': supplier
+    }
+    return render(request, 'invoiceparser/detail.html', context)
