3a2ac09d27f185424e3b4e4cec4f92e75984e462
Michael Berman <michael.v.berman@gmail.com>
2020-06-21 15:07:41 -0400

    add dynamic url for invoiceparser

--- invoiceparser/urls.py
+++ invoiceparser/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'invoiceparser'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:supplier_id>/', views.detail, name='detail')
