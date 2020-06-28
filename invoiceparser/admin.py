6ecb80a083783103274367cbd44c9663bd79d0fd
Michael Berman <michael.v.berman@gmail.com>
2020-06-28 14:18:06 -0400

    alter UI of admin add

--- invoiceparser/admin.py
+++ invoiceparser/admin.py
@@ -4,5 +4,24 @@ from django.contrib import admin
 
 from .models import Supplier, InvoiceItem
 
-admin.site.register(Supplier)
-admin.site.register(InvoiceItem)
+
+class SupplierAdmin(admin.ModelAdmin):
+    fields = ['supplier_name', 'supplier_location']
+
+
+class InvoiceItemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Supplier', {
+            "fields": (
+                ['supplier']
+            ),
+        }),
+        ('Invoice Item Information', {
+            'fields': (
+                ['description', 'price', 'ship_date']
+            )})
+    ]
+
+
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(InvoiceItem, InvoiceItemAdmin)
