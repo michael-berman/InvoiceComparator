ce4216b7bfe07d8db4f018da695f7108535b9772
Michael Berman <michael.v.berman@gmail.com>
2020-07-25 19:04:38 -0400

    add pdfplumber for parsing

--- invoiceparser/urls.py
+++ invoiceparser/urls.py
@@ -6,5 +6,6 @@ app_name = 'invoiceparser'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:supplier_id>/', views.detail, name='detail'),
-    path('<int:supplier_id>/create/', views.create, name='create')
+    path('<int:supplier_id>/create/', views.create, name='create'),
+    path('upload', views.upload_file, name="upload")
 ]
--- invoiceparser/views.py
+++ invoiceparser/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+import os
+import io
+import ocrmypdf
+import pdfplumber
+import pandas as pd
+from tempfile import TemporaryFile
+
 
 from .models import Supplier, InvoiceItem
-
-# Create your views here.
 
 
 def index(request):
@@ -46,3 +51,32 @@ def create(request, supplier_id):
         return render(request, 'invoiceparser/detail.html', context)
 
     return HttpResponseRedirect(reverse('invoiceparser:detail', args=(supplier_id,)))
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        invoice_file = request.FILES['invoice']
+        if request.FILES['invoice']:
+            invoice_text = _extract_text(invoice_file)
+
+    supplier_list = Supplier.objects.order_by('supplier_name')
+    context = {
+        'supplier_list': supplier_list,
+        'extracted_text': invoice_text
+    }
+    return render(request, 'invoiceparser/index.html', context)
+
+
+def _extract_text(invoice_file):
+    # Open a PDF file.
+    # path = '/Users/michaelberman/Documents/unread_invoice.pdf'
+    print(invoice_file.name)
+    new_file = None
+
+    with TemporaryFile() as f:
+        f.write(invoice_file.read())
+
+        ocrmypdf.ocr(invoice_file, invoice_file.name, deskew=True)
+        with pdfplumber.load(invoice_file) as pdf:
+            page = pdf.pages[0]
+            return page.extract_text()
