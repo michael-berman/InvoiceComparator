16bb84688cc350240dfbf3e6da0964216f00cb63
Michael Berman <michael.v.berman@gmail.com>
2020-07-27 19:26:30 -0400

    finish parsing johnstone

---
+++ invoiceparser/service_johnstone.py
@@ -0,0 +1,65 @@
+import re
+
+
+def parse_johnstone_invoice(invoice_text):
+    # Regular expressions for parsing
+    date_re = re.compile(r'([0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9])')
+    description_re = re.compile(r'(?i)(Description)')
+    each_re = re.compile(r'\d+ea \d+ea')
+    first_half_line_item_re = re.compile(r'(?i)(\d+ea \d+ea(.*))')
+    second_half_line_item_re = re.compile(r'(?i)(?:(?! \d+.\d+\/ea).)*')
+    price_raw_re = re.compile(r'(?i)\d+.\d+\/ea')
+    price_re_without_ea = re.compile(r'(?i)(?:(?!\/ea).)*')
+    subtotal_re = re.compile(r'(?i)(Subtotal)')
+
+    meta_data = []
+    line_items = []
+    ship_date = ""
+    invoice_number = ""
+
+    invoice_date_found = False
+    lines = invoice_text.split("\n")
+    for i in range(len(lines)):
+        line = lines[i]
+        if date_re.match(line) and invoice_date_found is False:
+            date, invoice_number = line.split(" ")
+
+            # OCR is reading S as $
+            meta_data.append("Invoice Date: " + date)
+            meta_data.append("Invoice Number: " + invoice_number)
+            invoice_date_found = True
+
+        if description_re.search(line):
+
+            # start scanning for description lines
+            current_item = ""
+            current_price = ""
+            for j in range(i + 1, len(lines)):
+                description_line = lines[j]
+
+                if subtotal_re.search(description_line):
+                    line_items.append((current_item, current_price))
+                    break
+
+                if each_re.search(description_line):
+                    current_item = re.search(
+                        first_half_line_item_re, description_line).group(2)
+                    current_item = re.search(
+                        second_half_line_item_re, current_item).group(0)
+
+                    if price_raw_re.search(description_line):
+                        current_price = re.search(
+                            price_raw_re, description_line).group(0)
+                        current_price = re.search(
+                            price_re_without_ea, current_price).group(0)
+                elif description_line.strip() != "":
+                    current_item += " " + description_line
+                    # meta_data.append("Description: " + current_item)
+                    line_items.append((current_item, current_price))
+
+            break
+
+    for line_item in line_items:
+        item, price = line_item
+        meta_data.append(item + " : " + price)
+    return meta_data
--- invoiceparser/services.py
+++ invoiceparser/services.py
@@ -1,18 +1,25 @@
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
 import os
 import io
 import re
 import ocrmypdf
+from shutil import copyfile
 import pdfplumber
 
 from .service_delta import parse_delta_invoice
+from .service_johnstone import parse_johnstone_invoice
 
 
 def save_line_items(invoice_file):
     invoice_text = convert_with_ocr(invoice_file)
-    os.remove(invoice_file.name)
+    if os.path.exists(invoice_file.name):
+        os.remove(invoice_file.name)
 
     # Regular expressions
-    delta_re = re.compile(r'^DELTA')
+    delta_re = re.compile(r'(?i)^DELTA')
+    johnstone_re = re.compile(r'(?i)(JOHNSTONE)')
 
     lines = invoice_text.split("\n")
     for i in range(len(lines)):
@@ -21,11 +28,20 @@ def save_line_items(invoice_file):
         if delta_re.match(line):
             return parse_delta_invoice(invoice_text)
 
+        if johnstone_re.match(line):
+            return parse_johnstone_invoice(invoice_text)
+
 
 def convert_with_ocr(invoice_file):
-    ocrmypdf.ocr(invoice_file.file, invoice_file.name, deskew=True)
-    temp_file = open(invoice_file.name, "r")
 
-    with pdfplumber.load(temp_file.buffer) as pdf:
-        page = pdf.pages[0]
-        return page.extract_text()
+    try:
+        ocrmypdf.ocr(invoice_file.file, invoice_file.name, deskew=True)
+        temp_file = open(invoice_file.name, "r")
+
+        with pdfplumber.load(temp_file.buffer) as pdf:
+            page = pdf.pages[0]
+            return page.extract_text()
+    except Exception:
+        with pdfplumber.load(invoice_file.file) as pdf:
+            page = pdf.pages[0]
+            return page.extract_text()
