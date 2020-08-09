c32745d7c91cdbbc3e3e89469209d57ed4fe56c2
Michael Berman <michael.v.berman@gmail.com>
2020-08-09 12:34:31 -0400

    add search for invoice items

--- invoiceparser/static/invoiceparser/scripts.js
+++ invoiceparser/static/invoiceparser/scripts.js
@@ -11,6 +11,7 @@ $('#invoiceFileInput').on('change', function () {
 $(".first-suppler-select-invoice-item").click(function () {
     $("#dropdownMenuButton1:first-child").text($(this).text());
     $("#dropdownMenuButton1:first-child").val($(this).text());
+    $("#dropdownMenuButton1:first-child").data('id', $(this).data("id"));
     $.ajax({
         url: "/invoiceparser/ajax/invoiceitems/" + $(this).data("id"),
         success: function (data) {
@@ -25,9 +26,25 @@ $(".first-suppler-select-invoice-item").click(function () {
 
 });
 
+$('#searchText1').on('input', function () {
+    const supplier_id = $("#dropdownMenuButton1:first-child").data('id');
+    $.ajax({
+        url: "/invoiceparser/ajax/invoiceitems/" + supplier_id + "/" + $(this).val(),
+        success: function (data) {
+            console.log(data)
+            const invoice_items = data.invoice_items;
+            var table = $('#firstTable tbody');
+            $('#firstTable tbody').empty();
+            for (let i = 0; i < invoice_items.length; i++)
+                table.append('<tr><td>' + invoice_items[i].description + '</td><td>' + invoice_items[i].price + '</td></tr>')
+        }
+    });
+});
+
 $(".second-suppler-select-invoice-item").click(function () {
     $("#dropdownMenuButton2:first-child").text($(this).text());
     $("#dropdownMenuButton2:first-child").val($(this).text());
+    $("#dropdownMenuButton2:first-child").data('id', $(this).data("id"));
     $.ajax({
         url: "/invoiceparser/ajax/invoiceitems/" + $(this).data("id"),
         success: function (data) {
@@ -40,6 +57,21 @@ $(".second-suppler-select-invoice-item").click(function () {
         }
     });
 
+});
+
+$('#searchText2').on('input', function () {
+    const supplier_id = $("#dropdownMenuButton2:first-child").data('id');
+    $.ajax({
+        url: "/invoiceparser/ajax/invoiceitems/" + supplier_id + "/" + $(this).val(),
+        success: function (data) {
+            console.log(data)
+            const invoice_items = data.invoice_items;
+            var table = $('#secondTable tbody');
+            $('#secondTable tbody').empty();
+            for (let i = 0; i < invoice_items.length; i++)
+                table.append('<tr><td>' + invoice_items[i].description + '</td><td>' + invoice_items[i].price + '</td></tr>')
+        }
+    });
 });
 
 $(".first-suppler-select-invoice").click(function () {
